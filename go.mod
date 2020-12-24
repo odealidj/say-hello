1b2c1c793fa5f7ddc54c3f36800a996b6419f153
odealidj <odealidj.go@gmail.com>
2020-12-24 07:04:00 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/odealidj/say-hello
+
+go 1.15
+
+require github.com/odealidj/mod-say-hello v1.0.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	mod_say_hello "github.com/odealidj/mod-say-hello"
+)
+
+func main()  {
+	fmt.Println(mod_say_hello.SayHello())
+
+}
